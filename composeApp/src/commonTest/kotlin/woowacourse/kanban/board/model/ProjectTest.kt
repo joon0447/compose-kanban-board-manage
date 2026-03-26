7fbbc8023db5060caa22341274c183c90fa32c5b
joon0447 <joon0447@naver.com>
2026-03-26 11:31:45 +0900

    test: 입력한 id를 가진 태스크 카드가 변경값으로 입력한 status로 변경된다

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/ProjectTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/ProjectTest.kt
@@ -13,6 +13,7 @@ import woowacourse.kanban.board.model.taskcard.Tags
 import woowacourse.kanban.board.model.taskcard.Tag
 import woowacourse.kanban.board.model.taskcard.Status
 import woowacourse.kanban.board.model.taskcard.Profile
+import kotlin.test.assertTrue
 
 
 class ProjectTest {
@@ -29,10 +30,28 @@ class ProjectTest {
                     status = Status.TODO,
                     profile = Profile("다이노",Res.drawable.profile),
                 )
-        )
+            )
         )
         project.updateTaskStatus("테스트", Status.PROGRESS)
         assertThat(project.todoTasks.size).isEqualTo(0)
         assertThat(project.progressTasks.size).isEqualTo(1)
+    }
+
+    fun `찾고자 하는 태스크 카드의 id값을 넣었을 때 해당 id 값을 가진 TaskCardData를 찾을 수 있다`() {
+        val task = TaskCardData(
+            id = "테스트",
+            title = Title("제목"),
+            description = Description("설명"),
+            tags = Tags(immutableListOf<Tag>(Tag("태그1"),Tag("태그1"))),
+            status = Status.TODO,
+            profile = Profile("다이노",Res.drawable.profile),
+        )
+        val project = Project(
+            title = "테스트 프로젝트",
+            initialTasks = immutableListOf<TaskCardData>(
+                task
+            )
+        )
+        assertTrue { project.findTaskById("테스트") == task }
     }
 }
