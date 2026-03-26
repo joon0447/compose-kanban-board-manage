656fc9377ef7b40f14f6ab36b67bb66eb63e6536
joon0447 <joon0447@naver.com>
2026-03-26 13:07:47 +0900

    test: 사이트탭에 등록된 프로젝트의 타이틀이 모두 출력한다

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/WorkSpaceTest.kt
@@ -0,0 +1,28 @@
+package woowacourse.kanban.board.component
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.runComposeUiTest
+import kotlinx.collections.immutable.toImmutableList
+import woowacourse.kanban.board.component.sample.ProjectPreviewData
+import woowacourse.kanban.board.model.state.WorkSpaceState
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class WorkSpaceTest {
+
+    @Test
+    fun `사이트탭에 등록된 프로젝트의 타이틀이 모두 출력된다`() = runComposeUiTest {
+    val projects = ProjectPreviewData().values.toImmutableList()
+        val workSpace = WorkSpaceState(projects)
+
+        setContent {
+            WorkSpace(workSpaceState = workSpace)
+        }
+
+        onNodeWithText("Compose1").assertIsDisplayed()
+        onNodeWithText("Compose2").assertIsDisplayed()
+        onNodeWithText("Compose3너무너무길경우에는 말줄임표로 표시됩니다.").assertIsDisplayed()
+    }
+}
